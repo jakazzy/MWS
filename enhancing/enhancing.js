a5be29eef8a96fc9dff8a1f6dd726f4ba7098f4f
jakazzy <jakazzy@gmail.com>
2019-06-16 21:41:29 +0000

    add funtion for fail and suceed

--- enhancing/enhancing.js
+++ enhancing/enhancing.js
@@ -6,10 +6,22 @@ module.exports = {
 };
 
 function succeed(item) {
+    if (item.enhancement < 20) {
+        item.enhancement++;
+    }
     return {...item };
 }
 
 function fail(item) {
+    if (item.enhancement < 15) {
+        durability = durability - 5;
+    }
+    if (item.enhancement > 16) {
+        durability--;
+    }
+    if (item.enhancement >= 15) {
+        durability = durability - 10;
+    }
     return {...item };
 }
 
