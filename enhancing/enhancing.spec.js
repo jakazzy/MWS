4843c67d4f54d67dd5bcf0383c64055d5242d193
jakazzy <jakazzy@gmail.com>
2019-06-17 22:23:40 +0000

    test repair function

--- enhancing/enhancing.spec.js
+++ enhancing/enhancing.spec.js
@@ -15,6 +15,14 @@ const item = {
 const success = succeed(item);
 const needRepair = repair(item);
 const failedItem = fail(item);
+
+describe('Repair item', () => {
+    it('should restore durability to 100', () => {
+        expect(needRepair.durability).toBe(100)
+    })
+});
+
+
 it('checks if value of item.enhancement less than zero increases by one', () => {
     expect(success.enhancement).toBe(15)
 })
