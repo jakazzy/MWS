ec5437865e135a9315fbe64ffe441ca5025e365e
jakazzy <jakazzy@gmail.com>
2019-06-22 13:52:49 +0000

    silence error with hack by KentCD

--- test-rtl-jest/src/fetch.test.js
+++ test-rtl-jest/src/fetch.test.js
@@ -4,11 +4,29 @@ import 'jest-dom/extend-expect'
 import axiosMock from 'axios'
 import Fetch from './Fetch'
 
+
+
+
 afterEach(cleanup)
 it('fetches and displays data', async() => {
 axiosMock.get.mockResolvedValueOnce( { data : { greeting: 'hello there'}});
 
-    const url = '/greeting';
-    const { getByTestId} = render( <Fetch url = { url }/>);
-    expect(getByTestId('loading')).toHaveTextContent('Loading data...');
+const url = '/greeting';
+const { getByTestId } = render(<Fetch url ={ url }/>);
+expect(getByTestId('loading')).toHaveTextContent('Loading data...');
+})
+
+//to silence warning
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
 })
