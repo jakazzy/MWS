9912900ef291e3a6f65af5d990691e0335be7a99
jakazzy <jakazzy@gmail.com>
2020-05-07 14:29:40 +0000

    add image filter

--- performance-optimization/web-workers/script.js
+++ performance-optimization/web-workers/script.js
@@ -1,23 +1,26 @@
 const $input = document.getElementById('input')
 const $preview = document.getElementById('preview')
-const previewCtr = $preview.getContext('2d')
+const previewCtx = $preview.getContext('2d')
 
 const fileReader = new FileReader()
 const image = new Image()
 
 function applyFilter(){
-    const imageData =previewCtr.getImageData(0,0, image.width, image.height)
-    for(let x =0; x< image.width; x++){
+    const imageData = previewCtx.getImageData(0,0, image.width, image.height)
+
+    for(let x = 0; x < image.width; x++){
         for(let y = 0; y < image.height; y++){
-            let index = (x +(y + image.width) * 4)
+            let index = (x + (y * image.width)) * 4
+            imageData.data[index + 3] = 127
         }
     }
+    previewCtx.putImageData(imageData, 0, 0)
 }
 
 image.addEventListener('load', (e)=>{
     $preview.width = image.width
     $preview.height = image.height
-    previewCtr.drawImage(image, 0,0)
+    previewCtx.drawImage(image, 0,0)
     applyFilter()
     
 })
