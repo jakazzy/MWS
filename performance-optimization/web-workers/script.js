da210e869ea9e85f313e5242fb44c443267a6c31
jakazzy <jakazzy@gmail.com>
2020-05-07 11:17:07 +0000

    add js and canvas

--- performance-optimization/web-workers/script.js
+++ performance-optimization/web-workers/script.js
@@ -1,4 +1,17 @@
 const $input = document.getElementById('input')
+const fileReader = new FileReader()
+const image = new Image()
+
+image.addEventListener('load', (e)=>{
+    document.body.appendChild(image)
+})
+
+fileReader.addEventListener('load', (e)=>{
+    const base64 = e.target.result
+    image.src = base64
+})
+
 $input.addEventListener('change', (e)=>{
-    console.log(e)
+    const file = e.target.files[0]
+    fileReader.readAsDataURL(file)
 })
