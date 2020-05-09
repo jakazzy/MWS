586bfba92c07dbd632c1bc2daf6f3260528bcda9
jakazzy <jakazzy@gmail.com>
2020-05-09 21:20:56 +0000

    edit index file

--- high-conv-forms/validation/password-validation/app.js
+++ high-conv-forms/validation/password-validation/app.js
@@ -16,39 +16,24 @@ match a character that isn't allowed in this password: /[^A-z0-9\!\@\#\$\%\^\&\*
 /*
 Grabbing a few inputs to help you get started...
  */
-
 var firstPasswordInput = document.querySelector('#first');
 var secondPasswordInput = document.querySelector('#second');
 var submit = document.querySelector('#submit');
-var firstValue = firstPasswordInput.value
-var secondValue = secondPasswordInput.value
+
 /*
 You'll probably find this function useful...
  */  
+submit.onclick = function () {
+    const firstpwd = firstPasswordInput.value
+    const secondpwd = secondPasswordInput.value
 
-submit.onclick = function(){
-    // console.log(typeof firstPasswordInput.value.length,  firstPasswordInput.value.length, ' password input value')
-    // event.preventDefault()
-    const str = /[!@#$%^&*]/g
-    const found = str.test('asd@')
-    console.log('testing regex: ',found)
-
-    if( firstPasswordInput.value === secondPasswordInput.value){
-        console.log("same here",  firstPasswordInput.value, secondPasswordInput.value)
-        secondPasswordInput.setCustomValidity("")
-    } else {
-        console.log("shut it")
-        secondPasswordInput.setCustomValidity("Password do not match")
-        return
+    
+    
+    let feedback = ""
+    if (firstpwd !== secondpwd ){
+        feedback = feedback + "First password must be same as second password"
     }
+    firstPasswordInput.setCustomValidity("something wrong here")
+   console.log( firstPasswordInput );
+};
 
-    if(firstPasswordInput.value.length < 16){
-
-        console.log(typeof firstPasswordInput.value.length, 'log first password input')
-        firstPasswordInput.setCustomValidity("Password should be greater than 16")
-        return
-    } else {
-        console.log("what have i done")
-        firstPasswordInput.setCustomValidity("")
-    }
-}
